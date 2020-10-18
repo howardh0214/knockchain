050a49b6c3dd1a5b9c45fbe1f3a72dceb25b8ba4
Howard <howardh0214@gmail.com>
2020-10-19 01:43:37 +0800

    Changed output message

--- src/NewChainz.java
+++ src/NewChainz.java
@@ -4,10 +4,10 @@ public class NewChainz {
 		Block genesisBlock = new Block("Hi im the first block", "0");
 		System.out.println("Hash for block 1 : " + genesisBlock.hash);
 		
-		Block secondBlock = new Block("Hi im the second block",genesisBlock.hash);
+		Block secondBlock = new Block("Ho im the second block",genesisBlock.hash);
 		System.out.println("Hash for block 2 : " + secondBlock.hash);
 		
-		Block thirdBlock = new Block("Hi im the third block",secondBlock.hash);
+		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
 		System.out.println("Hash for block 3 : " + thirdBlock.hash);
     }
 }
